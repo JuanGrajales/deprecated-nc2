a94a85a801740f611d53a9e2cc8bb043f5692014
JuanGrajales <inbox1.juan+github@gmail.com>
2021-04-10 13:05:20 -0400

    feat(react-w2): display comments

--- react/nucampsite/src/components/CampsiteInfoComponent.jsx
+++ react/nucampsite/src/components/CampsiteInfoComponent.jsx
@@ -15,10 +15,40 @@ class CampsiteInfo extends Component {
       </div>
     );
   }
+
+  renderComments(comments) {
+    if (comments) {
+      return (
+        <div className="col-md-5 m-1">
+          <h4>Comments</h4>
+          {comments.map((comment) => {
+            return (
+              <div>
+                <p>
+                  {comment.text} <br />
+                  -- {comment.author},{" "}
+                  {new Intl.DateTimeFormat("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "2-digit",
+                  }).format(new Date(Date.parse(comment.date)))}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      );
+    } else {
+      return <div>No comments</div>;
+    }
+  }
   render() {
     if (this.props.campsite) {
       return (
-        <div className="row">{this.renderCampsite(this.props.campsite)}</div>
+        <div className="row">
+          {this.renderCampsite(this.props.campsite)}{" "}
+          {this.renderComments(this.props.campsite.comments)}
+        </div>
       );
     } else {
       return <div>Nothing selected</div>;
